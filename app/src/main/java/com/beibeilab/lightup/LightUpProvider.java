49acc301d0fbe8dbc3a3a69517979708c1d4977c
David-Retina15 <david77115@gmail.com>
2017-06-03 22:12:49 +0800

    setup widget click event

--- app/src/main/java/com/beibeilab/lightup/LightUpProvider.java
+++ app/src/main/java/com/beibeilab/lightup/LightUpProvider.java
@@ -5,6 +5,8 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -12,6 +14,8 @@ import android.widget.RemoteViews;
  */
 
 public class LightUpProvider extends AppWidgetProvider {
+
+    public static final String ACTION_CLICK = "ACTION_CLICK";
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int N = appWidgetIds.length;
@@ -21,17 +25,31 @@ public class LightUpProvider extends AppWidgetProvider {
             int appWidgetId = appWidgetIds[i];
 
             // Create an Intent to launch ExampleActivity
-//            Intent intent = new Intent(context, ExampleActivity.class);
-//            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            Intent intent = new Intent(context, LightUpProvider.class);
+            intent.setAction(ACTION_CLICK);
+
+            //  set broadcast type PendingIntent
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
 
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
-//            views.setOnClickPendingIntent(R.id.button, pendingIntent);
+            views.setOnClickPendingIntent(R.id.image_button, pendingIntent);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction() != null) {    //	by broadcast
+            if(intent.getAction().equals(ACTION_CLICK)) {
+                Log.d("crazyma", "widget click!");
+            }
+        }
+        super.onReceive(context, intent);
     }
 
 }
