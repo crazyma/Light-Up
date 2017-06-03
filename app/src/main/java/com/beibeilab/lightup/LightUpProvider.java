dfadf95d3ed5139f1b71481d8158003040baf906
David-Retina15 <david77115@gmail.com>
2017-06-03 18:29:38 +0800

    widget first setting

---
+++ app/src/main/java/com/beibeilab/lightup/LightUpProvider.java
@@ -0,0 +1,10 @@
+package com.beibeilab.lightup;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by david on 2017/6/3.
+ */
+
+public class LightUpProvider extends AppWidgetProvider {
+}
