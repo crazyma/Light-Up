71872fd327bd1b63b3a64c9399ed4fd3bebf0520
David-Retina15 <david77115@gmail.com>
2017-06-03 19:16:15 +0800

    test brightness

--- app/src/main/java/com/beibeilab/lightup/MainActivity.java
+++ app/src/main/java/com/beibeilab/lightup/MainActivity.java
@@ -1,13 +1,90 @@
 package com.beibeilab.lightup;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variable to store brightness value
+    private int brightness;
+    //Content resolver used as a handle to the system's settings
+    private ContentResolver cResolver;
+    //Window object, that will store a reference to the current window
+    private Window window;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view) {
+
+        if(view.getId() == R.id.button) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.System.canWrite(this)) {
+                    // Do stuff here
+                    getBrightness();
+                } else {
+                    Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                    intent.setData(Uri.parse("package:" + getPackageName()));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        }else{
+            changeBrightness();
+        }
+
+    }
+
+    private void getBrightness(){
+        //Get the content resolver
+        cResolver = getContentResolver();
+
+        //Get the current window
+        window = getWindow();
+
+        try {
+            // To handle the auto
+            Settings.System.putInt(cResolver,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+            //Get the current system brightness
+            brightness = Settings.System.getInt(cResolver, Settings.System.SCREEN_BRIGHTNESS);
+
+            Log.d("crazyma", "brightness : " + brightness);
+        } catch (Settings.SettingNotFoundException e) {
+            //Throw an error case it couldn't be retrieved
+            Log.e("Error", "Cannot access system brightness");
+            e.printStackTrace();
+        }
+    }
+
+    private void changeBrightness(){
+        if(brightness == 0){
+            brightness = 255;
+        }else{
+            brightness = 0;
+        }
+        //Set the system brightness using the brightness variable value
+        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+
+        //Get the current window attributes
+        WindowManager.LayoutParams layoutpars = window.getAttributes();
+
+        //Set the brightness of this window
+        layoutpars.screenBrightness = brightness / (float)255;
+
+        //Apply attribute changes to this window
+        window.setAttributes(layoutpars);
     }
 }
